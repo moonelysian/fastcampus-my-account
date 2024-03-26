5c48612dca210db1b87601f69370508a0573f61b
moonelysian <yejimun.dev@gmail.com>
2024-03-26 20:56:37 +0900

    add: mock 데이터 추가

--- src/remote/firebase.ts
+++ src/remote/firebase.ts
@@ -13,7 +13,6 @@ const app =
         storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
         messagingSenderId: process.env.NEXT_PUBLIC_MESSAGEING_SENDER_ID,
         appId: process.env.NEXT_PUBLIC_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
       })
 
 export const auth = getAuth(app)
